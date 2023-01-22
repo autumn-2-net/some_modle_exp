97fc789f3d0dac296cdab166bfa466fec73269f2
autumn <2>
2023-01-22 22:03:56 +0800

    vec fi

--- libs/modle.py
+++ libs/modle.py
@@ -6,7 +6,6 @@ import torch.nn as nn
 import torchaudio
 from tqdm import tqdm
 import torch
-import torch.nn as nn
 
 import pytorch_lightning as pl
 import torch.nn.functional as F
@@ -15,6 +14,9 @@ from math import sqrt
 import numpy as np
 from pytorch_lightning import loggers as pl_loggers
 from torch.utils.tensorboard import SummaryWriter
+import torch.utils.data
+
+
 #
 # astc=torch.Tensor(1,40)
 # a=0
@@ -55,15 +57,15 @@ from torch.utils.tensorboard import SummaryWriter
 # print(ccd)
 
 def up_pad(x):
-    if x<36:
-        return 36,36-x
-    d=x-36
-    if d==0:
-        return 36,0
+    if x < 36:
+        return 36, 36 - x
+    d = x - 36
+    if d == 0:
+        return 36, 0
 
-    if d%8==0:
+    if d % 8 == 0:
         return x, 0
-    return x+8-d%8,8-d%8
+    return x + 8 - d % 8, 8 - d % 8
 
 
 class en_cov(nn.Module):
@@ -74,60 +76,102 @@ class en_cov(nn.Module):
         self.cov1d2 = nn.Conv1d(2, 4, kernel_size=6, stride=2)
         self.cov1d3 = nn.Conv1d(4, 6, kernel_size=6, stride=2)
 
-    def forward(self,x):
-        x=self.cov1d(x)
+    def forward(self, x):
+        x = self.cov1d(x)
         x = silu(x)
         x = self.cov1d2(x)
         x = silu(x)
         x = self.cov1d3(x)
         x = silu(x)
         return x
+
+
 @torch.jit.script
 def silu(x):
     return x * torch.sigmoid(x)
+
 
 class de_cov(nn.Module):
     def __init__(self):
         super().__init__()
 
-
         self.upc = nn.ConvTranspose1d(6, 4, kernel_size=6, stride=2)
         self.upc2 = nn.ConvTranspose1d(4, 2, kernel_size=6, stride=2)
         self.upc3 = nn.ConvTranspose1d(2, 1, kernel_size=6, stride=2)
-    def forward(self,x):
-        x=self.upc(x)
-        x=silu(x)
+
+    def forward(self, x):
+        x = self.upc(x)
+        x = silu(x)
         x = self.upc2(x)
         x = silu(x)
         x = self.upc3(x)
         x = silu(x)
         return x
 
+
 class vec_pl(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.ec=en_cov()
+        self.ec = en_cov()
         self.dc = de_cov()
 
         self.grad_norm = 0
-        self.loss=nn.L1Loss()
-
-
+        self.loss = nn.L1Loss()
+        self.step = 0
 
     def forward(self, x):
-        x=self.ec(x)
-        x=self.dc(x)
+        x = self.ec(x)
+        x = self.dc(x)
         return x
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.params.learning_rate)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=0.002)
         return optimizer
 
     def on_after_backward(self):
         self.grad_norm = nn.utils.clip_grad_norm_(self.parameters(), None or 1e9)
 
     def training_step(self, batch, batch_idx):
-        x,p=batch
+        x, p = batch
+        y = self.forward(x)
+
+        # b1 = beatch_cut(y, p)
+        # b2 = beatch_cut(x, p)
+        b1 = y
+        b2 = x
+        loss = self.loss(b1, b2)
+
+        self.step = self.step + 1
+        if self.step % 500 == 0:
+            self._write_summary(self.step, beatch_cut(x, p), beatch_cut(y, p), loss)
+
+        return loss
+
+    def _write_summary(self, step, insound, outs, loss):  # log 器
+        tensorboard = self.logger.experiment
+        # writer = tensorboard.SummaryWriter
+        writer = SummaryWriter("./mdss/", purge_step=step)
+        # writer = tensorboard
+        writer.add_audio('feature/audio', insound[0], step, sample_rate=self.params.sample_rate)
+        writer.add_audio('out/audio', outs[0], step, sample_rate=self.params.sample_rate)
+
+        writer.add_scalar('train/loss', loss, step)
+        writer.add_scalar('train/grad_norm', self.grad_norm, step)
+        writer.flush()
+        self.summary_writer = writer
+
+
+def beatch_cut(bt, pdd: list):
+    act = []
+
+    blcn = bt.size()[0]
+    blc = torch.chunk(bt, blcn, dim=0)
+    for idx, i in enumerate(blc):
+        icp = -int(pdd[idx])
+        act.append(i.squeeze(0).squeeze(0)[:icp].unsqueeze(0).unsqueeze(0))
+
+    return tuple(act)
+
 
 class ConditionalDataset(torch.utils.data.Dataset):
     def __init__(self, paths):
@@ -136,7 +180,6 @@ class ConditionalDataset(torch.utils.data.Dataset):
         for path in paths:
             print(paths, path)
             self.filenames += glob(f'{path}/**/*.wav', recursive=True)
-
 
     def __len__(self):
         return len(self.filenames)
@@ -151,30 +194,29 @@ class ConditionalDataset(torch.utils.data.Dataset):
 
         }
 
+
 def make_time(time):
-    upc=up_pad(time)
-    if random.randint(1,10)==4:
+    upc = up_pad(time)
+    if random.randint(1, 10) == 4:
         return time
-    if upc[1]==0:
-        return time-random.randint(1,7)
-    full=upc[0]
-    return full-random.randint(1,7)
+    if upc[1] == 0:
+        return time - random.randint(1, 7)
+    full = upc[0]
+    return full - random.randint(1, 7)
+
+
 class Collator:
-    def __init__(self, params):
-        self.params = params
 
     def collate(self, minibatch):
-        samples_per_frame = self.params.hop_samples
-        time =int(random.randint(1,2)*random.random()*44100)
-        pplsdc=[]
 
+        time = int(random.randint(1, 2) * random.random() * 44100)
+        pplsdc = []
 
         for record in minibatch:
-            rpct=make_time(time)
+            rpct = make_time(time)
             pdc = up_pad(rpct)
             # Filter out records that aren't long enough.
-            if len(record['audio']) < 44100*2.5:
-                del record['spectrogram']
+            if len(record['audio']) < 44100 * 2.5:
                 del record['audio']
                 continue
 
@@ -184,10 +226,15 @@ class Collator:
             record['audio'] = np.pad(record['audio'], (0, pdc[1]), mode='constant')
             pplsdc.append(pdc[1])
 
-
         audio = np.stack([record['audio'] for record in minibatch if 'audio' in record])
+        cdts = []
+        for iee in minibatch:
+            if 'audio' in iee:
+                cdts.append(torch.from_numpy(iee['audio']).unsqueeze(0))
 
-        return torch.from_numpy(audio), torch.tensor(pplsdc)
+        sss = torch.cat(tuple(cdts), dim=0).unsqueeze(1)
+
+        return sss, torch.tensor(pplsdc)
 
         #
         # return {
@@ -198,30 +245,26 @@ class Collator:
     # for gtzan
 
 
-
-
-
-def from_path(data_dirs, params, is_distributed=False):
+def from_path(data_dirs, is_distributed=False):
     dataset = ConditionalDataset(data_dirs)
     return torch.utils.data.DataLoader(
         dataset,
         batch_size=48,
-        collate_fn=Collator(params).collate,
+        collate_fn=Collator().collate,
         shuffle=not is_distributed,
         # num_workers=os.cpu_count(),
         num_workers=2,
 
-
         pin_memory=True,
-        drop_last=False #flase hao hai shi?
+        drop_last=False  # flase hao hai shi?
     )
 
 
+if __name__ == "__main__":
+    torch.backends.cudnn.benchmark = True
+    md = vec_pl()
+    tensorboard = pl_loggers.TensorBoardLogger(save_dir="", name='sdxc')
+    dataset = from_path([r'C:\Users\autumn\Desktop\poject_all\vcoder\testwav'], )
 
-
-
-
-
-
-
-
+    trainer = pl.Trainer(max_epochs=100, logger=tensorboard, gpus=1)
+    trainer.fit(model=md, train_dataloader=dataset)
